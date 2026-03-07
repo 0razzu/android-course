40cf9b16ee6453928ecfa3150450966b26366ba6
0razzu <0razzu@protonmail.com>
2026-03-07 17:13:38 +0600

    rememberSaveable

--- app/src/main/java/io/orazzu/android_course/MainActivity.kt
+++ app/src/main/java/io/orazzu/android_course/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -49,7 +50,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TextPasser(modifier: Modifier = Modifier) {
     val ctx = LocalContext.current
-    var text by remember { mutableStateOf("Abc") }
+    var text by rememberSaveable { mutableStateOf("Abc") }
 
     Column(
         modifier = modifier

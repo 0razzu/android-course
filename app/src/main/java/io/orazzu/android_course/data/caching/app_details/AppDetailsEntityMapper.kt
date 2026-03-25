2cbcd3ca581a8d004229c2600ce630869f73dbee
0razzu <0razzu@protonmail.com>
2026-03-25 19:36:24 +0600

    removed ()

--- app/src/main/java/io/orazzu/android_course/data/caching/app_details/AppDetailsEntityMapper.kt
+++ app/src/main/java/io/orazzu/android_course/data/caching/app_details/AppDetailsEntityMapper.kt
@@ -12,7 +12,7 @@ class AppDetailsEntityMapper @Inject constructor() {
         ageRating = domain.ageRating,
         iconUrl = domain.iconUrl,
         screenshots = domain.screenshotUrlList,
-        description = (domain.longDescription ?: domain.shortDescription)!!,
+        description = domain.longDescription ?: domain.shortDescription!!,
     )
 
     fun toDomain(entity: AppDetailsEntity): AppDetails = AppDetails(

0ffa23e19acec1157fe76505c44095d23b0f6c78
0razzu <0razzu@protonmail.com>
2026-03-30 00:33:55 +0600

    AppDetailsDao: fixed putAppDetails()

--- app/src/main/java/io/orazzu/android_course/data/caching/app_details/AppDetailsDao.kt
+++ app/src/main/java/io/orazzu/android_course/data/caching/app_details/AppDetailsDao.kt
@@ -1,9 +1,8 @@
 package io.orazzu.android_course.data.caching.app_details
 
 import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import io.orazzu.android_course.domain.AppCategory
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -11,9 +10,63 @@ interface AppDetailsDao {
     @Query("select * from app_details where id = :id")
     fun getAppDetails(id: String): Flow<AppDetailsEntity?>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    // TODO do not replace wishlist status
-    fun putAppDetails(appDetails: AppDetailsEntity)
+    @Query(
+        """
+    insert into app_details (
+        id,
+        name,
+        developer,
+        category,
+        ageRating,
+        iconUrl,
+        screenshots,
+        description,
+        isInWishlist
+    )
+    values (
+        :id,
+        :name,
+        :developer,
+        :category,
+        :ageRating,
+        :iconUrl,
+        :screenshots,
+        :description,
+        :isInWishlist
+    )
+    on conflict(id) do update set
+        name = excluded.name,
+        developer = excluded.developer,
+        category = excluded.category,
+        ageRating = excluded.ageRating,
+        iconUrl = excluded.iconUrl,
+        screenshots = excluded.screenshots,
+        description = excluded.description
+    """,  // preserve isInWishlist
+    )
+    fun putAppDetailsInternal(
+        id: String,
+        name: String,
+        developer: String,
+        category: AppCategory,
+        ageRating: Int,
+        iconUrl: String,
+        screenshots: List<String>,
+        description: String?,
+        isInWishlist: Boolean,
+    )
+
+    fun putAppDetails(appDetails: AppDetailsEntity) = putAppDetailsInternal(
+        appDetails.id,
+        appDetails.name,
+        appDetails.developer,
+        appDetails.category,
+        appDetails.ageRating,
+        appDetails.iconUrl,
+        appDetails.screenshots,
+        appDetails.description,
+        appDetails.isInWishlist,
+    )
 
     @Query("update app_details set isInWishlist = :isInWishlist where id = :id")
     fun updateWishlistStatus(id: String, isInWishlist: Boolean)

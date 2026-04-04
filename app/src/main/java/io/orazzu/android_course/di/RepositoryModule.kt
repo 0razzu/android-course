2076146cf3e08cd4c59b78ca8841ab6496e97e6e
0razzu <0razzu@protonmail.com>
2026-04-04 22:43:39 +0600

    added @Singleton

--- app/src/main/java/io/orazzu/android_course/di/RepositoryModule.kt
+++ app/src/main/java/io/orazzu/android_course/di/RepositoryModule.kt
@@ -8,13 +8,16 @@ import io.orazzu.android_course.data.app.AppLocalRepo
 import io.orazzu.android_course.data.app_details.AppDetailsLocalRepo
 import io.orazzu.android_course.domain.app.AppRepo
 import io.orazzu.android_course.domain.app_details.AppDetailsRepo
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
     @Binds
+    @Singleton
     abstract fun bindAppDetailsRepo(impl: AppDetailsLocalRepo): AppDetailsRepo
 
     @Binds
+    @Singleton
     abstract fun bindAppRepo(impl: AppLocalRepo): AppRepo
 }
